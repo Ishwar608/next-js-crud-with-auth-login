37424ecf97e0de112b8f75b0d07a09d864819d13
Ishwar Suthar <90741501+Ishwar608@users.noreply.github.com>
2024-08-03 12:31:17 +0530

    Add the sign up router

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import SignUpForm from "./SignUpForm";
 
 export default function page() {
   return (
-    <main className="flex h-full w-full items-center justify-center">
-      <h1>Sign Up</h1>
-      <p>This is the Sign Up page.</p>
+    <main className="mt-12">
+      <div className="mx-auto w-1/3 rounded-2xl border bg-gray-200 p-4 shadow-sm dark:bg-card">
+        <h1 className="mb-5 text-2xl font-semibold">😍 Sign Up</h1>
+        <SignUpForm />
+      </div>
     </main>
   );
 }
--- src/lib/validation-form/authentication.ts
+++ src/lib/validation-form/authentication.ts
@@ -4,3 +4,8 @@ export const loginFormSchema = z.object({
   email: z.string().min(1, "This field is required"),
   password: z.string().min(1, "This field is required"),
 });
+
+export const signUpFormSchema = z.object({
+  email: z.string().min(1, "This field is required"),
+  password: z.string().min(1, "This field is required"),
+});
